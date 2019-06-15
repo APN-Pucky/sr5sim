d0d664f2c4634601549a91f1d1eb160e48c26504
APN-Pucky <APN-Pucky@users.noreply.github.com>
2019-06-15 20:37:55 +0200

    Linux compatibility

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
 rm -rf build
 mkdir -p build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=.. ..
+cmake ..
 cmake --build . 
 cmake --install .
 ctest -C Debug
-echo "----------------------------"
-../bin/sr5sim.exe

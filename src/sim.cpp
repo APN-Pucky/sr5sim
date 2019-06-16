f70a9f9bc54467723b6d48cb5eae73ef780e55c6
APN-Pucky <APN-Pucky@users.noreply.github.com>
2019-06-17 01:14:17 +0200

    sim more test

--- src/sim.cpp
+++ src/sim.cpp
@@ -1,8 +1,10 @@
 #include <vector>
+#include <iostream>
 #include <initializer_list>
 #include <map>
 #include <algorithm>
 #include "sim.h"
+#include "debug.h"
 
 class Field
 {
@@ -16,9 +18,18 @@ public:
 void new_init_round(Field& f)
 {
 	//new initiative
-	for(auto ci : f.cis)
+	for(CI& ci : f.cis)
 	{
 		ci.init();
+	}
+}
+
+void next_round(Field& f)
+{
+	//new initiative
+	for(CI& ci : f.cis)
+	{
+		ci.has_acted =false;
 	}
 }
 
@@ -26,17 +37,33 @@ void simulate(SimData& sd,std::initializer_list<Character> chars)
 {
 	Field f(chars,sd);
 	f.cis[0].uid = 1;
-	new_init_round(f);
-	sort(f.cis,greater<CI>);
-	
-	while(!f.cis[0].has_acted && f.cis[0].current_initiative()>0)
+
+	while(!f.cis[0].ko() && !f.cis[1].ko())
 	{
-		f.cis[0].act(f.cis);
-		f.cis[0].current_init_roll -=10;
-		sort(f.cis.begin(),f.cis.end(),greater<CI>);		
+		new_init_round(f);
+		sort(f.cis.begin(),f.cis.end());
+		while(f.cis[0].current_initiative()>0)
+		{
+			_DEBUG_MSG(2,"ROUND:\n");
+			while(!f.cis[0].has_acted )
+			{
+				if(f.cis[0].current_initiative()>0 ) {
+					f.cis[0].act(f.cis);
+					f.cis[0].current_init_roll -=10;
+				}
+				else
+				{
+					f.cis[0].has_acted = true;
+				}
+				sort(f.cis.begin(),f.cis.end());		
+			}
+			next_round(f);
+			// and re-sort
+			sort(f.cis.begin(),f.cis.end());		
+		}
 	}
 	
 	f.simdata.res=STALL;
 	for(auto a : f.cis)
 		if(a.uid==1)f.simdata.res = a.ko()?LOSS:WIN;
-};
+}
